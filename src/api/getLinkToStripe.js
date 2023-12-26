6e59ad00b0c07e6ee5e8bdf8ba7ed28cf393c38e
sm4rtdev <elonscottdev@gmail.com>
2023-12-26 11:19:02 -0400

    Merge

--- src/api/getLinkToStripe.js
+++ src/api/getLinkToStripe.js
@@ -1,11 +1,7 @@
 import { customFetch } from "./customFetch";
 
-export const getLinkAccount = async (pasarela) => {
-  if (pasarela === "paypal") {
-    const pathname = `api/meta-usuario/paypal-connect`;
-    return await customFetch(pathname);
-  } else {
-    const pathname = `api/meta-usuario/stripe-connect`;
-    return await customFetch(pathname);
-  }
+export const getLinkToStripe = async () => {
+  const pathname = `api/meta-usuario/stripe-connect`;
+
+  return await customFetch(pathname);
 };
--- src/pages/Private/Instructor/PaymentMethods/PaymentMethods.jsx
+++ src/pages/Private/Instructor/PaymentMethods/PaymentMethods.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
 import "./PaymentMethods.scss";
+
+import { LogoPaypal, LogoStripe } from "../../../../assets/images";
+
 import InstructorHeader from "../../../../components/CustomHeaders/InstructorHeader";
 import PageTransition from "../../../../components/PageTransition/PageTransition";
-import Footer from "../../../../components/Footer/Footer";
-import { LogoPaypal, LogoStripe } from "../../../../assets/images";
-import { getLinkAccount } from "../../../../api/getLinkStripeAccount";
-import { toast } from "react-toastify";
 import SpinnerOfDoom from "../../../../components/SpinnerOfDoom/SpinnerOfDoom";
 import Footer from "../../../../components/Footer/Footer";
 import { getAvailablePaymentMethods } from "../../../../api/getAvailablePaymentMethods";
@@ -14,33 +15,81 @@ import { getLinkToPaypal } from "../../../../api/getLinkToPaypal";
 import { getUserMetadata } from "../../../../api/getUserMetadata";
 
 const PaymentMethods = () => {
-  const [loadingStripe, setLoadingStripe] = useState(false);
-  const [loadingPaypal, setLoadingPaypal] = useState(false);
+  const [isLoading, setIsLoading] = useState({
+    ip: true,
+    meta: true,
+    paypal: false,
+    stripe: false,
+  });
 
-  const getLink = async (pasarela) => {
-    if (pasarela === "stripe") {
-      setLoadingStripe(true);
-    } else if (pasarela === "paypal") {
-      setLoadingPaypal(true);
-    }
+  const [availableMethods, setAvailableMethods] = useState({
+    paypal: false,
+    stripe: false,
+  });
+  const [linkedMethods, setLinkedMethods] = useState({
+    paypal: false,
+    stripe: false,
+  });
 
-    const { ok, data } = await getLinkAccount(pasarela);
-    if (ok) {
-      if (pasarela === "stripe") {
-        window.location = data.link.url;
+  useEffect(() => {
+    // Check already linked methods
+    (async () => {
+      const { ok, data } = await getUserMetadata();
+
+      if (ok) {
+        // console.log(data.data.attributes);
+
+        const { stripe_account_id_state, paypal_account_id } =
+          data.data.attributes;
+        // console.log(stripe_account_id_state, paypal_account_id);
+
+        setLinkedMethods({ stripe_account_id_state, paypal_account_id });
       } else {
-        // muestro un toast indicando que se vinculo correctamente el email
-
-        toast.success(`Se vinculó correctamente paypal al email ${data.email}`);
+        toast.error(`${data.error.message}`);
       }
+
+      setIsLoading((current) => ({ ...current, meta: false }));
+    })();
+
+    // Check available methods based on IP
+    (async () => {
+      const { ok, data } = await getAvailablePaymentMethods();
+
+      if (ok) {
+        const methods = data.data.data;
+
+        setAvailableMethods((current) => ({ ...current, ...methods }));
+      } else {
+        toast.error(`${data.error.message}`);
+      }
+
+      setIsLoading((current) => ({ ...current, ip: false }));
+    })();
+  }, []);
+
+  const linkToPaypal = async () => {
+    setIsLoading((current) => ({ ...current, paypal: true }));
+
+    const { ok, data } = await getLinkToPaypal();
+
+    if (ok) {
+      toast.success(`Success`);
     } else {
       toast.error(`${data.error.message}`);
     }
 
-    if (pasarela === "stripe") {
-      setLoadingStripe(false);
-    } else if (pasarela === "paypal") {
-      setLoadingPaypal(false);
+    setIsLoading((current) => ({ ...current, paypal: false }));
+  };
+  const linkToStripe = async () => {
+    setIsLoading((current) => ({ ...current, stripe: true }));
+
+    const { ok, data } = await getLinkToStripe();
+
+    if (ok) {
+      window.open(data.link.url, "_self");
+    } else {
+      toast.error(`${data.error.message}`);
+      setIsLoading((current) => ({ ...current, stripe: false }));
     }
   };
 
@@ -48,39 +97,78 @@ const PaymentMethods = () => {
     <div id="payment-methods" className="page">
       <PageTransition>
         <InstructorHeader />
+
         <div className="main">
           <h1>Métodos de pago</h1>
-          <div className="method">
-            <div className="image">
-              <LogoStripe />
-            </div>
-            <button className="small-button" onClick={() => getLink("stripe")} disabled={loadingStripe}>
-              {loadingStripe ? (
-                <>
-                  <SpinnerOfDoom />
-                  Cargando...
-                </>
-              ) : (
-                "Conectar"
+
+          {isLoading.meta || isLoading.ip ? (
+            <SpinnerOfDoom standalone full />
+          ) : (
+            <>
+              {(linkedMethods.paypal_account_id !== null ||
+                availableMethods.paypal) && (
+                <div className="method">
+                  <div className="image">
+                    <LogoPaypal />
+                  </div>
+
+                  <div className="text">
+                    <p>
+                      Al hacer clic en el botón, el correo de PayPal se
+                      configurará automáticamente con la misma dirección de
+                      correo que tu cuenta de Brane.
+                    </p>
+
+                    {linkedMethods.paypal_account_id && (
+                      <p>
+                        Actualmente el correo configurado es:
+                        <strong>{` ${linkedMethods.paypal_account_id}`}</strong>
+                      </p>
+                    )}
+                  </div>
+
+                  <button
+                    className="action-button black"
+                    onClick={linkToPaypal}
+                    disabled={
+                      linkedMethods.paypal_account_id !== null ||
+                      isLoading.paypal
+                    }
+                  >
+                    {isLoading.paypal && <SpinnerOfDoom />}
+                    {linkedMethods.paypal_account_id !== null
+                      ? "Vinculado"
+                      : "Vincular"}
+                  </button>
+                </div>
               )}
-            </button>
-          </div>
-          <div className="method">
-            <div className="image">
-              <LogoPaypal />
-            </div>
-            <button className="small-button" onClick={() => getLink("paypal")} disabled={loadingPaypal}>
-              {loadingPaypal ? (
-                <>
-                  <SpinnerOfDoom />
-                  Cargando...
-                </>
-              ) : (
-                "Conectar"
+
+              {(linkedMethods.stripe_account_id_state === "completed" ||
+                availableMethods.stripe) && (
+                <div className="method">
+                  <div className="image">
+                    <LogoStripe />
+                  </div>
+
+                  <button
+                    className="action-button black"
+                    onClick={linkToStripe}
+                    disabled={
+                      linkedMethods.stripe_account_id_state === "completed" ||
+                      isLoading.stripe
+                    }
+                  >
+                    {isLoading.stripe && <SpinnerOfDoom />}
+                    {linkedMethods.stripe_account_id_state === "completed"
+                      ? "Vinculado"
+                      : "Vincular"}
+                  </button>
+                </div>
               )}
-            </button>
-          </div>
+            </>
+          )}
         </div>
+
         <Footer unique instructor />
       </PageTransition>
     </div>
